88f87f8c1fc6d4a3af25e4182cd34ab948eec8a7
Joseph Perry <josephperry720@gmail.com>
2020-10-28 19:12:29 -0400

    added header component to table

---
+++ src/components/Table/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <tr>
+      <th>Image</th>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Email</th>
+      <th>DOB</th>
+    </tr>
+  );
+};
+
+export default Header;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Row from "./Row";
+import Header from "./Header";
 import seeds from "./seeds.json";
 
 class Table extends Component {
   render() {
     return (
       <table>
+        <thead>
+          <Header/>
+        </thead>
         <tbody>
           {seeds.map((person) => (
             <Row
