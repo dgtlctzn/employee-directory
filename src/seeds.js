77891835117814c64ec1f220350f8f5bd06a809f
Joseph Perry <josephperry720@gmail.com>
2020-10-28 16:33:09 -0400

    filtered through api results and wrote json file

--- src/seeds.js
+++ src/seeds.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
-// import fs from "fs";
+const fs = require("fs");
 
 const url = "https://randomuser.me/api/?results=20";
 
+// call to random user api to get seeds
 (async function toJson() {
   try {
     const {data} = await axios({
@@ -10,7 +11,24 @@ const url = "https://randomuser.me/api/?results=20";
       url: url,
       responseType: "json",
     });
-    console.log(data);
+    const personas = data.results;
+    // simplifies the random user api results to only applicable categories
+    modPersonas = [];
+    for (const person of personas) {
+        const newPersona = {
+            image: person.picture.thumbnail,
+            name: `${person.name.first} ${person.name.last}`,
+            phone: person.phone,
+            email: person.email,
+            dob: person.dob.date
+        }
+        modPersonas.push(newPersona);
+    }
+    fs.writeFile("./src/seeds.json", JSON.stringify(modPersonas, null, 2), err => {
+        if (err) {
+            console.log(err)
+        }
+    })
   } catch (err) {
     console.log(err);
   }
