926c8b2f000a97bcc754462ea98f429a23e4184e
Joseph Perry <josephperry720@gmail.com>
2020-10-29 16:47:05 -0400

    added hover and icon style

--- src/components/Table/Header.jsx
+++ src/components/Table/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({handleNameSort, icon}) => {
   return (
     <tr>
       <th onClick={()=> {console.log("click")}}>Image</th>
-      <th onClick={()=> {handleNameSort()}}>Name<i className={icon}></i></th>
+      <th id="name-sort" onClick={()=> {handleNameSort()}}>Name<i className={icon}></i></th>
       <th onClick={()=> {console.log("click")}}>Phone</th>
       <th onClick={()=> {console.log("click")}}>Email</th>
       <th onClick={()=> {console.log("click")}}>DOB</th>
